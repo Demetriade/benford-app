1dc5de784da55be9769218ce195e57179e05e874
Charles-Antoine Demetriade <ca.demetriade@outlook.com>
2020-12-26 12:01:26 -0500

    Others

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,20 +1,67 @@
 import React from 'react'
-import MyDropzoneArea from "./MyDropzoneArea"
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {DropzoneArea} from 'material-ui-dropzone'
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import axios from "axios"
 import "./Home.css"
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiDropzoneArea: {
+      root: {
+        backgroundColor: "#3c3e42",
+        width: "75vw",
+        height: "100px",
+        marginRight: "auto",
+        marginLeft: "auto"
+      },
+      icon: {
+        color: "white"
+      }
+    },
+  }
+});
 
 const Home = () => {
+  const [image, setImage] = React.useState();
+
+  const handleChange = (newFile) => {
+    setImage(newFile)
+    console.log(image)
+  }
+  
+  const sendImage = () => {
+    console.log("image sent")
+    let formData = new FormData()
+    formData.append("uploadedImage", image)
+    /*axios.post("http://localhost:3001/", formData)
+              .then(response => { console.log(response.data) })
+              .catch(error => { console.log(error) })*/
+  }
+
   return (
     <div className="home">
         <Typography variant="h3" className="title">
             Try it!
         </Typography>
         <hr />
-        <Typography variant="p" className="explication">
-            You can select an image from your device or select one from Instagram to see the degree of fakeness
+        <Typography variant="caption" className="explication" style={{display: 'inline-block', fontSize: "medium"}}>
+            Select an image from your device to see the degree of fakeness. The result may not be allways right.
         </Typography>
-        <MyDropzoneArea />
+        
+        <MuiThemeProvider theme={theme}>
+          <DropzoneArea
+            onDrop={handleChange}
+            showFileNames
+            dropzoneText="Drag and drop an image here or click"
+            showAlerts={true}
+            filesLimit={1} />
+        </MuiThemeProvider>
+
+        <Button variant="contained" color="primary" className="sendButton" onClick={sendImage}>
+          Process
+        </Button>
     </div>
   );
 }
--- frontend/src/components/What.js
+++ frontend/src/components/What.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import "./What.css";
+import imageUrl from "../static/images/benf.PNG"
+
 
 const What = () => {
     return (
@@ -10,15 +16,28 @@ const What = () => {
         </Typography>
         <hr />
         <div className="explication">
-          <Typography variant="p" style={{display: 'inline-block'}}>
+          <Typography variant="caption" style={{display: 'inline-block', fontSize: "medium"}}>
               In the world of fraud detection, there is a mathemetic law that is use a lot. This one may look like magic at first look, but it is a little more complex.
               Of course, this is Benford's law. The basic explication are if we take the first digit of every numbers in a data set, then we calculate the pourcentage of
               their occurence and sort them by digit order, the pourcentage of ones will be around 30%, the other pourcentage will decrease until the digit nine that his pourcentage will be around 5 pourcent.
               (Look the graph under) 
           </Typography>
+          
           <br />
-          <Typography variant="p" style={{display: 'inline-block'}}>
-              blablablablablablablablablabla 
+
+          <Card className="cardImg">
+            <CardHeader title="The distribution of the digits" />
+            <CardMedia className="media" image={imageUrl} title="Benford's law" style={{ height: "15hv", width: "10wv" }} component="img" />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                From https://en.wikipedia.org/wiki/Benford's_law.
+              </Typography>
+            </CardContent>
+          </Card>
+
+          <Typography variant="caption" style={{display: 'inline-block', fontSize: "medium"}}>
+              On this site, we can experiment this law with images. More precisely, if we take the first digit of every pixel color codes, then we are able to apply
+              benford's law to see the distribution of digits. This is one of many application of this particular law. 
           </Typography>
         </div>
       </div>
